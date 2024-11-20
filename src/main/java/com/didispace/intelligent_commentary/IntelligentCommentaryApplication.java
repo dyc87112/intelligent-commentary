f0b1b9ce406d5b2161608dad10614dd898e87f8d
程序猿DD <dyc87112@qq.com>
2024-11-20 10:06:34 +0800

    init

---
+++ src/main/java/com/didispace/intelligent_commentary/IntelligentCommentaryApplication.java
@@ -0,0 +1,13 @@
+package com.didispace.intelligent_commentary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentCommentaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentCommentaryApplication.class, args);
+	}
+
+}
